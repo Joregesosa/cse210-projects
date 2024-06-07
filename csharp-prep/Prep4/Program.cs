9926e7f51b4509274095f59a1b59b9e5e7734d4e
Jorge Sosa <joregesosa@gmail.com>
2024-06-07 14:04:26 -0400

    complete prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+
+        int number = 0;
+
+        do
+        {
+            Console.Write("Enter a number (0 to stop): ");
+            number = int.Parse(Console.ReadLine());
+
+            numbers.Add(number);
+        } while (number != 0);
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        double average = (double)sum / numbers.Count;
+
+        int max = 0;
+        foreach (int num in numbers)
+        {
+            if (num > max && num > 0)
+            {
+                max = num;
+            }
+        }
+        numbers.Sort();
+        string sortedNumbers = string.Join(", ", numbers);
+
+
+
+
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Average: {average}");
+        Console.WriteLine($"Maximum: {max}");
+        Console.WriteLine($"Sorted numbers: {sortedNumbers}");
+
     }
 }
