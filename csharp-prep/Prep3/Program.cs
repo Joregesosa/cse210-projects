03a3c26fcecc4d91c9b68970663a9a88b6ab99a0
Jorge Sosa <joregesosa@gmail.com>
2024-06-07 13:34:49 -0400

    complete prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(1, 10);
+         
+        int guess = 0;
+        int attempts = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("Enter your guess: ");
+            guess = int.Parse(Console.ReadLine());
+            attempts++;
+
+            if (guess == magicNumber)
+            {
+                Console.WriteLine($"Congratulations! You guessed the magic number in {attempts} attempts.");
+                
+                Console.Write("Would you like to play again? (yes/no): ");
+                string playAgain = Console.ReadLine();
+                if (playAgain == "yes")
+                {
+                    magicNumber = randomNumber.Next(1, 10);
+                    attempts = 0;
+                }
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Your guess is too low. Try again.");
+            }
+            else
+            {
+                Console.WriteLine("Your guess is too high. Try again.");
+            }
+        }
     }
 }
